fc88c7b3faac8a4331c3a0f7b89e54b801512294
Christopher R. Eady <ChrisMac@christophersmbp.att.net>
2014-08-21 21:00:36 -0500

    added a test class

---
+++ GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Christopher R. Eady on 8/21/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Christopher R. Eady on 8/21/14.
+//
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
