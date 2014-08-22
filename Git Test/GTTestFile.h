bbc77315a3ca85a2fa912ffde1dc23044f0314e2
Christopher R. Eady <ChrisMac@christophersmbp.att.net>
2014-08-21 21:15:41 -0500

    corrected file locations

---
+++ Git Test/GTTestFile.h
@@ -0,0 +1,13 @@
+//
+//  GTTestFile.h
+//  Git Test
+//
+//  Created by Christopher R. Eady on 8/21/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTestFile : NSObject
+
+@end
---
+++ Git Test/GTTestFile.m
@@ -0,0 +1,13 @@
+//
+//  GTTestFile.m
+//  Git Test
+//
+//  Created by Christopher R. Eady on 8/21/14.
+//
+//
+
+#import "GTTestFile.h"
+
+@implementation GTTestFile
+
+@end
